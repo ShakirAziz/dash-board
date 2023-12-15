5d251ef0942f3b1d8bcdafa9ce7901b92904e730
ShakirAziz <shakiraziz686@gmail.com>
2023-12-15 17:09:11 +0500

    ADD: FOLDER Structure

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum atque
+        dolorem voluptates ut cum consequuntur molestias ad esse? Aliquid,
+        error. Nihil autem ea hic facere alias velit fugit omnis magnam.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
