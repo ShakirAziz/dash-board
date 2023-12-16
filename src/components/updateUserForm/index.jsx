afb2efd3f4c19b69e25bff2df107d027df1bb46f
muzahir1 <muzahir45678@gmail.com>
2023-12-16 17:58:37 +0500

    Add: react icon to createUserForm

--- src/components/updateUserForm/index.jsx
+++ src/components/updateUserForm/index.jsx
@@ -1,7 +1,18 @@
+import FormInputs from "../formInputs/index";
+
+import { FaUserEdit } from "react-icons/fa";
 const UpdateUserForm = () => {
   return (
     <>
-      <h1 style={{ color: "white" }}>Here add update user Form</h1>
+      <form>
+        <FormInputs
+          type="text"
+          id="1"
+          name="username"
+          iconUrl={<FaUserEdit />}
+          placeholder="user name (to be update)"
+        />
+      </form>
     </>
   );
 };
