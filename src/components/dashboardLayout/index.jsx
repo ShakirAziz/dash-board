c984517c02524ec893f809ad649dfe3b970ecca1
ShakirAziz <shakiraziz686@gmail.com>
2023-12-18 15:41:07 +0500

    Add: AddUser component and resolve conflicts

--- src/components/dashboardLayout/index.jsx
+++ src/components/dashboardLayout/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./styles.css";
+import AddUser from "../addUser";
 const DashboardMainLayout = () => {
-  return <div className="mainContainer"></div>;
+  return (
+    <div className="mainContainer">
+      <AddUser />
+    </div>
+  );
 };
 
 export default DashboardMainLayout;
