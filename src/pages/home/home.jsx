8c3cef432cdef6dc5163a642331743c5ec08df18
ShakirAziz <shakiraziz686@gmail.com>
2023-12-19 13:18:21 +0500

    Add: patient managment components

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Dashboard from "../../layouts/dashboradLayout/dashboard";
-import PageComponent from "../../components/landingPage/landing";
+
 import Routers from "../../routes/Routes";
 const Home = () => {
   return <Routers />;
