f6fdf9be6de8d3fb010c747df3ffc2a5749c4a73
ShakirAziz <shakiraziz686@gmail.com>
2023-12-19 16:57:34 +0500

    Add: clinical management components

---
+++ src/components/addClinicLocation/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddClinicLocation = () => {
+  return (
+    <div>
+      <h1>This is add clinic loctaion component</h1>
+    </div>
+  );
+};
+
+export default AddClinicLocation;
---
+++ src/components/updateClinicLocation/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdateClinicLocation = () => {
+  return (
+    <div>
+      <h1>This is update clinic location</h1>
+    </div>
+  );
+};
+
+export default UpdateClinicLocation;
