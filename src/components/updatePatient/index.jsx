d703dfd5c08b728013c9be18921f331b53bc109a
muzahir1 <muzahir45678@gmail.com>
2023-12-19 16:51:33 +0500

    Add:add patient and update patient components to patient managment

--- src/components/updatePatient/index.jsx
+++ src/components/updatePatient/index.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import FormInputs from "../formInputs/index";
+import { FaUser } from "react-icons/fa";
+import { FaAddressCard } from "react-icons/fa6";
+import { MdMeetingRoom } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import FormButton from "../formButton/index";
 
 const UpdatePatient = () => {
   return (
-    <div>
-      <h1>This is update patient component </h1>
-    </div>
+    <>
+      <form>
+        <FormInputs
+          type="text"
+          id="1"
+          placeholder="Patient Name (to be update)"
+          iconUrl={<FaUser />}
+        />
+        <FormInputs
+          type="number"
+          id="2"
+          placeholder="Patient ID (to be update)"
+          iconUrl={<FaAddressCard />}
+        />
+        <FormInputs
+          type="number"
+          id="3"
+          placeholder="Patient Room Number (to be update)"
+          iconUrl={<MdMeetingRoom />}
+        />
+        <FormInputs
+          type="number"
+          id="4"
+          placeholder="Patient Phone Number (to be update)"
+          iconUrl={<FaPhoneAlt />}
+        />
+        <FormInputs
+          type="text"
+          id="5"
+          placeholder="Patient Attendant Name (to be update)"
+          iconUrl={<FaUser />}
+        />
+        <FormButton button="Update" />
+      </form>
+    </>
   );
 };
 
