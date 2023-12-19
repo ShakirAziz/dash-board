fefb75617ca97994e7a56299e66a77163f04d04c
muzahir1 <muzahir45678@gmail.com>
2023-12-19 18:18:51 +0500

    Add:add clinic location page

--- src/components/addClinicLocation/index.jsx
+++ src/components/addClinicLocation/index.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import FormInputs from "../formInputs/index";
+import { BiClinic } from "react-icons/bi";
+import { GrLocation } from "react-icons/gr";
+import FormButton from "../formButton/index";
 
 const AddClinicLocation = () => {
   return (
-    <div>
-      <h1>This is add clinic loctaion component</h1>
-    </div>
+    <>
+      <form>
+        <FormInputs
+          type="text"
+          id="1"
+          placeholder="Clinic  Name"
+          iconUrl={<BiClinic />}
+        />
+        <FormInputs
+          type="text"
+          id="2"
+          placeholder="Clinic Previous Address"
+          iconUrl={<GrLocation />}
+        />
+        <FormInputs
+          type="text"
+          id="3"
+          placeholder="Clinic New Address"
+          iconUrl={<GrLocation />}
+        />
+        <FormButton button="Add New Location" />
+      </form>
+    </>
   );
 };
 
