0b2407aabfba50ae2c1571555d7c25e0e4ac5858
muzahir1 <muzahir45678@gmail.com>
2023-12-19 17:49:33 +0500

    Add:add patient update location and delete location info pag

--- src/components/deleteClinicLocation/index.jsx
+++ src/components/deleteClinicLocation/index.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { GrLocation } from "react-icons/gr";
+import { BiClinic } from "react-icons/bi";
 
+import FormInputs from "../formInputs";
+import FormButton from "../formButton/index";
 const DeleteClinicLoctaion = () => {
   return (
-    <div>
-      <h1>This ic delete clinic loation</h1>
-    </div>
+    <>
+      <form>
+        <FormInputs
+          type="text"
+          id="1"
+          placeholder="Clinic Name"
+          iconUrl={<BiClinic />}
+        />
+        <FormInputs
+          type="text"
+          id="2"
+          placeholder="Clinic Location"
+          iconUrl={<GrLocation />}
+        />
+        <FormButton button="Delete Location" />
+      </form>
+    </>
   );
 };
 
